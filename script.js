26e186f56e4190559f4d7b583d01c59ead1b2766
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-15 16:33:36 +0200

    struttura e script iniziale

---
+++ script.js
@@ -0,0 +1,48 @@
+var root = new Vue(
+    {
+        el:'#root',
+        data:{
+            images:[
+                {
+                    src:'https://images.unsplash.com/photo-1604580864964-0462f5d5b1a8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cm9tYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+                    alt:''
+                },
+                {
+                    src:'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+                    alt:''
+                },
+                {
+                    src:'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__340.jpg',
+                    alt:''
+                },
+                {
+                    src:'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3RvY2slMjAlMjB3YWxscGFwZXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+                    alt:''
+                },
+            ],
+            imageIndex: 0,
+        },
+        methods:{
+            nextImage: function(){
+                this.imageIndex++;
+
+                console.log(this.imageIndex);
+
+                if(this.imageIndex >= this.images.length){
+                    this.imageIndex = 0;
+                }
+            },
+            previousImage: function(){
+                this.imageIndex--;
+
+                console.log(this.imageIndex);
+
+                if(this.imageIndex < 0){
+                    this.imageIndex = this.images.length - 1;
+                }
+                
+            }
+            
+        }
+    }
+);
